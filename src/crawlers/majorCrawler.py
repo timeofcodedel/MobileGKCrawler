a577f845e369784599dae091748b81bd756ebdc0
流光墨佰 <54519513+timeofcodedel@users.noreply.github.com>
2024-07-18 23:21:53 +0800

    Initial commit

---
+++ src/crawlers/majorCrawler.py
@@ -0,0 +1,177 @@
+import pandas
+import time
+
+from selenium.webdriver.remote.webelement import WebElement
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+class MajorCrawler:
+    def __init__(self):
+        self._chromeOptions = Options()
+        self._chromeOptions.add_experimental_option("detach", True)
+        # self._chromeOptions.add_argument("--headless")
+        self._PAGE_NUMBER = 148
+        self._chromeDrive = webdriver.Chrome(options=self._chromeOptions)
+        self._MAJOR_NAME: list = [
+            "综合类",
+            "理工类",
+            "农林类",
+            "医药类",
+            "师范类",
+            "语言类",
+            "财经类",
+            "政法类",
+            "体育类",
+            "艺术类",
+            "民族类",
+            "军事类",
+            "其他类",
+        ]
+
+    def _getWebSource(self) -> None:
+        self._chromeDrive.get("https://www.gaokao.cn/school/search")
+        time.sleep(15)  # 登录时间，后面在扩展
+        self._web_source = self._chromeDrive.page_source
+
+    def crawl(self) -> None:
+        schoolDataList: list[dict] = []
+        for pageNum in range(2, self._PAGE_NUMBER + 1):
+            pageCssSeletor: str = (
+                f"li[title='{pageNum}'].ant-pagination-item.ant-pagination-item-{pageNum}"
+            )
+            dataCssSelector: str = "school-search_schoolItem__3q7R2"
+            webElements: list[WebElement] = self._chromeDrive.find_elements(
+                By.CLASS_NAME, dataCssSelector
+            )
+            for element in webElements:
+                # print(element.text+'\n')
+                schoolDataList.append(self.dataParse(element.text))
+            self._chromeDrive.find_element(By.CSS_SELECTOR, pageCssSeletor).click()
+            time.sleep(0.4)
+            self._web_source = self._chromeDrive.page_source
+            print(pageNum)
+            if pageNum == self._PAGE_NUMBER - 1:
+                break
+        self.export(self._classifySchoolTypes(schoolDataList, mode="p"), mode="p")
+
+    def _classifySchoolTypes(
+        self, schoolList: list[dict], mode="a"
+    ) -> list[dict] | list:
+        _comprehensiveTypeList: list = []
+        _scienceAEngineerList: list = []
+        _agricultureAForestryList: list = []
+        _medicineList: list = []
+        _teacherTrainList: list = []
+        _languagTypeList: list = []
+        _financeAEconomicsTypeList: list = []
+        _politicsALawTypeList: list = []
+        _sportsTypeList: list = []
+        _artTypeList: list = []
+        _ethnicityTypeList: list = []
+        _militaryTypeList: list = []
+        _otherTypeList: list = []
+        for school in schoolList:
+            match school["type"]:
+                case "综合类":
+                    _comprehensiveTypeList.append(school)
+                case "理工类":
+                    _scienceAEngineerList.append(school)
+                case "农林类":
+                    _agricultureAForestryList.append(school)
+                case "医药类":
+                    _medicineList.append(school)
+                case "师范类":
+                    _teacherTrainList.append(school)
+                case "语言类":
+                    _languagTypeList.append(school)
+                case "财经类":
+                    _financeAEconomicsTypeList.append(school)
+                case "政法类":
+                    _politicsALawTypeList.append(school)
+                case "体育类":
+                    _sportsTypeList.append(school)
+                case "艺术类":
+                    _artTypeList.append(school)
+                case "民族类":
+                    _ethnicityTypeList.append(school)
+                case "军事类":
+                    _militaryTypeList.append(school)
+                case "其他类":
+                    _otherTypeList.append(school)
+        if mode is "a":
+            resultList: tuple = (
+                _comprehensiveTypeList
+                + _scienceAEngineerList
+                + _agricultureAForestryList
+                + _medicineList
+                + _teacherTrainList
+                + _languagTypeList
+                + _financeAEconomicsTypeList
+                + _politicsALawTypeList
+                + _sportsTypeList
+                + _artTypeList
+                + _ethnicityTypeList
+                + _militaryTypeList
+                + _otherTypeList
+            )
+            return resultList
+        elif mode is "p":
+            return [
+                _comprehensiveTypeList,
+                _scienceAEngineerList,
+                _agricultureAForestryList,
+                _medicineList,
+                _teacherTrainList,
+                _languagTypeList,
+                _financeAEconomicsTypeList,
+                _politicsALawTypeList,
+                _sportsTypeList,
+                _artTypeList,
+                _ethnicityTypeList,
+                _militaryTypeList,
+                _otherTypeList,
+            ]
+        else:
+            raise ValueError("请输入正确的模式")
+
+    def dataParse(self, dataStr: str) -> dict:
+        dataList = dataStr.split("\n")
+        # print(dataList)
+        schoolDict: dict = {
+            "name": dataList[0],
+            "local": dataList[1],
+            "level": dataList[2],
+            "type": dataList[3],
+            "feature": dataList[4],
+        }
+        temptags = ["985", "211", "双一流", "强基计划"]
+        tempList = []
+        for tag in temptags:
+            if tag in dataList:
+                tempList.append(tag)
+        if len(tempList) == 0:
+            schoolDict["universityStauts"] = None
+        else:
+            schoolDict["universityStauts"] = ",".join(tempList)
+        return schoolDict
+
+    def export(self, data: list, mode="a") -> None:
+        if mode == "a":
+            table = pandas.DataFrame(data)
+            with pandas.ExcelWriter("./院校总表", engine="xlsxwriter") as w:
+                table.to_excel(excel_writer=w, sheet_name="总表数据")
+        elif mode == "p":
+            with pandas.ExcelWriter("./院校总表.xlsx", engine="xlsxwriter") as w:
+                for part, index in zip(data, range(0, 13)):
+                    table = pandas.DataFrame(part)
+                    table.to_excel(excel_writer=w, sheet_name=self._MAJOR_NAME[index])
+        else:
+            raise ValueError("请输入正确的模式")
+
+
+if __name__ == "__main__":
+    majorCrawler = MajorCrawler()
+    majorCrawler._getWebSource()
+    majorCrawler.crawl()
